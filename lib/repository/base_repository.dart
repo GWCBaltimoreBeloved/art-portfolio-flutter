0d758da86d21cd2fb6b48865c24036af54e09642
Micaiah Skolnick <mskolnickdev@gmail.com>
2021-12-12 21:20:10 -0500

    add path to repository logs

--- lib/repository/base_repository.dart
+++ lib/repository/base_repository.dart
@@ -7,9 +7,11 @@ class BaseRepository<T> {
   late final T Function(DocumentSnapshot<Map<String, dynamic>> document)
       fromDocument;
   late final Map<String, dynamic> Function(T model) toJson;
+  final String collectionName;
+  String get _path => '/$collectionName';
 
   BaseRepository({
-    required String collectionName,
+    required this.collectionName,
     required T Function(Map<String, dynamic> json) fromJson,
     required Map<String, dynamic> Function(T model) toJson,
   }) {
@@ -31,20 +33,20 @@ class BaseRepository<T> {
     try {
       final response = await collectionReference.add(item);
       final data = response.get().then((value) => value.data());
-      log('Added successfully: ${item.toString()}');
+      log('$_path Added successfully: ${item.toString()}');
       return data;
     } catch (e) {
-      log('Error - Failed to add ${item.runtimeType} with error: $e');
+      log('$_path Error - Failed to add with error: $e');
     }
   }
 
   Future<bool> updateItem({required T item, required String documentId}) async {
     try {
       collectionReference.doc(documentId).update(toJson(item));
-      log('Updated successfully: ${item.toString()}');
+      log('$_path Updated successfully: ${item.toString()}');
       return true;
     } catch (e) {
-      log('Error - Failed to add ${item.runtimeType} with error: $e');
+      log('$_path Error - Failed to add ${item.runtimeType} with error: $e');
       return false;
     }
   }
@@ -52,9 +54,9 @@ class BaseRepository<T> {
   Future<T?> deleteItem({required String documentId}) async {
     try {
       await collectionReference.doc(documentId).delete();
-      log('Document $documentId deleted successfully');
+      log('$_path Document $documentId deleted successfully');
     } catch (e) {
-      log('Error - Failed to delete item of documentId: $documentId with error: $e');
+      log('$_path Error - Failed to delete item of documentId: $documentId with error: $e');
     }
   }
 
@@ -63,7 +65,7 @@ class BaseRepository<T> {
       final snapshots = await collectionReference.get();
       return snapshots.docs.map((e) => e.data()).toList();
     } catch (e) {
-      log('Error getting list: $e');
+      log('$_path Error getting list: $e');
     }
   }
 }
