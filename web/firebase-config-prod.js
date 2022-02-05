4226c5fd7444dcfc3ce11512e787954be2ab38d7
Micaiah Skolnick <mskolnickdev@gmail.com>
2022-02-05 10:37:21 -0500

    add prod index.html and script to brand

---
+++ web/firebase-config-prod.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0KkP5i8Pb7uLn-0KwVvdgrztg6Uc2ciE",
+    authDomain: "art-portfolio-flutter-prod.firebaseapp.com",
+    projectId: "art-portfolio-flutter-prod",
+    storageBucket: "art-portfolio-flutter-prod.appspot.com",
+    messagingSenderId: "878129769453",
+    appId: "1:878129769453:web:a319f2855949e248c3d5e4",
+    measurementId: "G-H7LTNFF1QZ"
+};
