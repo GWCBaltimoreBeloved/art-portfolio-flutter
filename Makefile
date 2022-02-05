89a31a01c4c0f25536ad959e2d27d7716f62dcc1
Micaiah Skolnick <mskolnickdev@gmail.com>
2022-02-05 12:57:24 -0500

    add page title to developer page

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ pod_update:
 	cd ios && pod update && flutter clean && flutter pub get
 
 run_prod:
-	flutter run --flavor prod -t lib/main-prod.dart 
+	flutter run --flavor prod -t lib/main_prod.dart 
 
 run_dev:
-	flutter run --flavor dev -t lib/main-dev.dart 
+	flutter run --flavor dev -t lib/main_dev.dart 
--- lib/pages/developer/developer_page.dart
+++ lib/pages/developer/developer_page.dart
@@ -14,60 +14,63 @@ class DeveloperPage extends ConsumerWidget {
       appBar: AppBar(
         title: Text('Flutter app. Flavor is: ${F.appFlavor}'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              child: Text('Login Page'),
-              onPressed: () => GoRouter.of(context).push(
-                MyRouter.routes.login,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _PageHeader(),
+              SizedBox(height: 24),
+              ElevatedButton(
+                child: Text('Login Page'),
+                onPressed: () => GoRouter.of(context).push(
+                  MyRouter.routes.login,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            ElevatedButton(
-              child: Text('Artist Registration Page'),
-              onPressed: () => GoRouter.of(context).push(
-                MyRouter.routes.artistRegistration,
+              SizedBox(height: 8),
+              ElevatedButton(
+                child: Text('Artist Registration Page'),
+                onPressed: () => GoRouter.of(context).push(
+                  MyRouter.routes.artistRegistration,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            ElevatedButton(
-              child: Text('Artist List Page'),
-              onPressed: () => GoRouter.of(context).push(
-                MyRouter.routes.artistList,
+              SizedBox(height: 8),
+              ElevatedButton(
+                child: Text('Artist List Page'),
+                onPressed: () => GoRouter.of(context).push(
+                  MyRouter.routes.artistList,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            ElevatedButton(
-              child: Text('Art List Page'),
-              onPressed: () => GoRouter.of(context).push(
-                MyRouter.routes.artList,
+              SizedBox(height: 8),
+              ElevatedButton(
+                child: Text('Art List Page'),
+                onPressed: () => GoRouter.of(context).push(
+                  MyRouter.routes.artList,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            ElevatedButton(
-              child: Text('Create Account Page'),
-              onPressed: () => GoRouter.of(context).push(
-                MyRouter.routes.createAccount,
+              SizedBox(height: 8),
+              ElevatedButton(
+                child: Text('Create Account Page'),
+                onPressed: () => GoRouter.of(context).push(
+                  MyRouter.routes.createAccount,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            ElevatedButton(
-              child: Text('Saved Art'),
-              onPressed: () => GoRouter.of(context).push(
-                MyRouter.routes.savedArt,
+              SizedBox(height: 8),
+              ElevatedButton(
+                child: Text('Saved Art'),
+                onPressed: () => GoRouter.of(context).push(
+                  MyRouter.routes.savedArt,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            ElevatedButton(
-              child: Text('Email Verification Page'),
-              onPressed: () => GoRouter.of(context).push(
-                MyRouter.routes.emailVerification,
+              SizedBox(height: 8),
+              ElevatedButton(
+                child: Text('Email Verification Page'),
+                onPressed: () => GoRouter.of(context).push(
+                  MyRouter.routes.emailVerification,
+                ),
               ),
-            ),
-            SizedBox(height: 8),
-            ElevatedButton(
+              SizedBox(height: 8),
+              ElevatedButton(
                 child: Text('Show loading dialog'),
                 onPressed: () async {
                   final closeDialog = showLoadingDialog(
@@ -76,10 +79,25 @@ class DeveloperPage extends ConsumerWidget {
                   );
                   await Future.delayed(Duration(seconds: 3));
                   closeDialog();
-                }),
-          ],
+                },
+              ),
+              SizedBox(height: 24),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _PageHeader extends StatelessWidget {
+  const _PageHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Developer Components Page',
+      style: TextStyle(fontSize: 36),
     );
   }
 }
