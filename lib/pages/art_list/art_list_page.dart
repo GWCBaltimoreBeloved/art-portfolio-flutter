e7fecbe9127d916c83de3cfab87c4bb8320e5d59
Micaiah Skolnick <mskolnickdev@gmail.com>
2022-02-19 11:59:50 -0500

    begin creating art list page

--- lib/pages/art_list/art_list_page.dart
+++ lib/pages/art_list/art_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:art_portfolio_flutter/my_router.dart';
+import 'package:art_portfolio_flutter/pages/art_list/art_list_provider.dart';
+import 'package:art_portfolio_flutter/repository/art/models/art.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -17,9 +19,51 @@ class ArtListPage extends ConsumerWidget {
                 : () => GoRouter.of(context).go(MyRouter.routes.developer),
             child: Text('Art List Page')),
       ),
-      body: Center(
-        child: Text('Art list page'),
-      ),
+      body: _PageBody(),
+    );
+  }
+}
+
+class _PageBody extends ConsumerWidget {
+  const _PageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(artListProvider);
+
+    return ListView.builder(
+      itemCount: provider.artList.length,
+      itemBuilder: (context, index) {
+        final artItem = provider.artList[index];
+        return _ListItem(artItem);
+      },
+    );
+  }
+}
+
+class _ListItem extends ConsumerWidget {
+  final Art artItem;
+  const _ListItem(this.artItem, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(
+      children: [
+        Text(
+          artItem.name,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          artItem.description,
+          style: TextStyle(
+            fontSize: 14,
+          ),
+        ),
+        Text(artItem.artist.firstName),
+      ],
     );
   }
 }
--- lib/pages/art_list/art_list_provider.dart
+++ lib/pages/art_list/art_list_provider.dart
@@ -1,3 +1,5 @@
+import 'package:art_portfolio_flutter/repository/art/models/art.dart';
+import 'package:art_portfolio_flutter/repository/artist/models/artist.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -5,4 +7,35 @@ final artListProvider = ChangeNotifierProvider.autoDispose((_) {
   return ArtListProvider();
 });
 
-class ArtListProvider extends ChangeNotifier {}
+class ArtListProvider extends ChangeNotifier {
+  final artList = [
+    Art(
+      name: 'name 1',
+      description: 'description 1',
+      downloadUrl: 'downloadUrl',
+      firebaseStoragePath: 'firebaseStoragePath',
+      artist: Artist(
+        userId: 'userId',
+        firstName: 'firstName 1',
+        lastName: 'lastName 1',
+        description: 'description',
+        instagram: 'instagram',
+        github: 'github',
+      ),
+    ),
+    Art(
+      name: 'name 2',
+      description: 'description 2',
+      downloadUrl: 'downloadUrl',
+      firebaseStoragePath: 'firebaseStoragePath',
+      artist: Artist(
+        userId: 'userId',
+        firstName: 'firstName 2',
+        lastName: 'lastName 2',
+        description: 'description',
+        instagram: 'instagram',
+        github: 'github',
+      ),
+    ),
+  ];
+}
